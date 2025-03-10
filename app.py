74712c39010e7246a2b7ec1fdad7ea3d6e694ac5
Jukelyn <viperkillergaming@gmail.com>
2025-03-10 16:59:52 -0400

    Fixed option to switch hemispheres

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request, send_from_directory
-import pandas as pd
+# pylint: disable=E0401, C0114, C0116
 import logging
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__, static_folder='static')
 
 logging.basicConfig(level=logging.DEBUG)
-STATIC_IMAGES_PATH = 'static/images'
 
 # Load fish data
 df = pd.read_csv("data/fish_datasheet.csv")
+
+CURRENT_IMAGE = "static/images/NH_spawning_calendar.png"
 
 
 # Home page route
@@ -17,17 +19,18 @@ def index():
     logging.debug("Handling request to '/' route")
     # List all fish names (for dropdown or filtering)
     all_fishes = df["Name"].dropna().unique().tolist()
-    image_url = f"{STATIC_IMAGES_PATH}/NH_spawning_calendar.png"
 
+    global CURRENT_IMAGE  # pylint: disable=W0603
     if request.method == 'POST':
         logging.debug("Received POST request")
-        hemisphere = request.form.get('hemisphere', 'NH')
-
-        filename = f"{STATIC_IMAGES_PATH}/{hemisphere}_spawning_calendar.png"
-        image_url = filename
+        button = request.form.get("hemisphere")
+        if button == "NH":
+            CURRENT_IMAGE = "static/images/NH_spawning_calendar.png"
+        elif button == "SH":
+            CURRENT_IMAGE = "static/images/SH_spawning_calendar.png"
 
     return render_template('index.html', fish_list=all_fishes,
-                           image_url=image_url)
+                           image_url=CURRENT_IMAGE)
 
 
 if __name__ == '__main__':
