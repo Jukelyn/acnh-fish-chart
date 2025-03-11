66953515ca8f806eac2fd82676429ba84cbc5901
Jukelyn <viperkillergaming@gmail.com>
2025-03-11 15:07:18 -0400

    stupid black linter (black is the name of the linter btw)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder="static")
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -16,20 +16,45 @@ logging.basicConfig(level=logging.DEBUG)
 df = pd.read_csv("data/fish_datasheet.csv")
 
 # Relevant columns for NH_df
-NH_columns = ["Name", "NH Jan", "NH Feb", "NH Mar", "NH Apr", "NH May",
-              "NH Jun", "NH Jul", "NH Aug", "NH Sep", "NH Oct", "NH Nov",
-              "NH Dec"]
+NH_columns = [
+    "Name",
+    "NH Jan",
+    "NH Feb",
+    "NH Mar",
+    "NH Apr",
+    "NH May",
+    "NH Jun",
+    "NH Jul",
+    "NH Aug",
+    "NH Sep",
+    "NH Oct",
+    "NH Nov",
+    "NH Dec"
+]
 NH_df = df[NH_columns].copy()
 
 # Relevant columns for SH_df
-SH_columns = ["Name", "SH Jan", "SH Feb", "SH Mar", "SH Apr", "SH May",
-              "SH Jun", "SH Jul", "SH Aug", "SH Sep", "SH Oct", "SH Nov",
-              "SH Dec"]
+SH_columns = [
+    "Name",
+    "SH Jan",
+    "SH Feb",
+    "SH Mar",
+    "SH Apr",
+    "SH May",
+    "SH Jun",
+    "SH Jul",
+    "SH Aug",
+    "SH Sep",
+    "SH Oct",
+    "SH Nov",
+    "SH Dec"
+]
 SH_df = df[SH_columns].copy()
 
 
-def plot_spawning_calendar(dataframe: pd.DataFrame, title: str,
-                           filename: str) -> None:
+def plot_spawning_calendar(
+    dataframe: pd.DataFrame, title: str, filename: str
+) -> None:
     """Creates a plot for the fish in a calendar style and saves it as image.
 
     Args:
@@ -44,9 +69,21 @@ def plot_spawning_calendar(dataframe: pd.DataFrame, title: str,
 
     ax = sns.heatmap(spawn_data, cmap="Greens", linewidths=0.5, cbar=False)
 
-    ax.set_xticklabels(["January", "February", "March", "April", "May", "June",
-                       "July", "August", "September", "October", "November",
-                        "December"])
+    ax.set_xticklabels(
+        ["January",
+         "February",
+         "March",
+         "April",
+         "May",
+         "June",
+         "July",
+         "August",
+         "September",
+         "October",
+         "November",
+         "December"
+         ]
+    )
 
     plt.xlabel("")
     ax.xaxis.set_ticks_position("top")
@@ -54,12 +91,12 @@ def plot_spawning_calendar(dataframe: pd.DataFrame, title: str,
 
     # Add a red line between the columns of the current month
     current_month = datetime.now().month
-    ax.axvline(x=current_month - 0.5, color='red', linestyle='-', linewidth=2)
+    ax.axvline(x=current_month - 0.5, color="red", linestyle="-", linewidth=2)
     # Create a custom legend
     legend_elements = [
-        Line2D([0], [0], color='red', lw=2, label='Current Month')]
+        Line2D([0], [0], color="red", lw=2, label="Current Month")]
     ax.legend(handles=legend_elements,
-              loc='upper right', bbox_to_anchor=(1.2, 1))
+              loc="upper right", bbox_to_anchor=(1.2, 1))
 
     plt.ylabel("")
     plt.title(title, loc="center")
@@ -77,11 +114,12 @@ plot_spawning_calendar(NH_df, "Northern Hemisphere",
 plot_spawning_calendar(SH_df, "Southern Hemisphere",
                        "SH_spawning_calendar.png")
 all_fishes: list[str] = list(df["Name"].dropna().unique())
-renamed_fish: dict[str, str] = {"pop eyed goldfish": "pop-eyed goldfish",
-                                "soft shelled turtle": "soft-shelled turtle",
-                                "napoleonfish": "Napoleonfish",
-                                "mahi mahi": "mahi-mahi"
-                                }
+renamed_fish: dict[str, str] = {
+    "pop eyed goldfish": "pop-eyed goldfish",
+    "soft shelled turtle": "soft-shelled turtle",
+    "napoleonfish": "Napoleonfish",
+    "mahi mahi": "mahi-mahi"
+}
 
 
 # Change this to get inputs from browser instead
@@ -95,7 +133,7 @@ def get_caught_fish(fishes_caught: list[str]) -> list[str]:
     caught_items = set()
 
     for fishy in fishes_caught:
-        fish_name = fishy.strip().replace('_', ' ')
+        fish_name = fishy.strip().replace("_", " ")
         fish_name = renamed_fish.get(fish_name, fish_name)
 
         caught_items.add(fish_name)
@@ -106,8 +144,8 @@ def get_caught_fish(fishes_caught: list[str]) -> list[str]:
 def process_fish_data(input_fish_list=None):
     caught_fish = get_caught_fish(input_fish_list or [])
     uncaught_fish = [fish for fish in all_fishes if fish not in caught_fish]
-    df_nh_uncaught = NH_df[NH_df['Name'].isin(uncaught_fish)].copy()
-    df_sh_uncaught = SH_df[SH_df['Name'].isin(uncaught_fish)].copy()
+    df_nh_uncaught = NH_df[NH_df["Name"].isin(uncaught_fish)].copy()
+    df_sh_uncaught = SH_df[SH_df["Name"].isin(uncaught_fish)].copy()
     return caught_fish, uncaught_fish, df_nh_uncaught, df_sh_uncaught
 
 
@@ -117,11 +155,11 @@ CURRENT_IMAGE = "static/images/NH_spawning_calendar.png"
 all_fish_list = df["Name"].dropna().unique().tolist()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     logging.debug("Handling request to '/' route")
     global CURRENT_IMAGE  # pylint: disable=W0603
-    if request.method == 'POST':
+    if request.method == "POST":
         logging.debug("Received POST request")
         button = request.form.get("hemisphere")
         if button == "NH":
@@ -129,14 +167,12 @@ def index():
         elif button == "SH":
             CURRENT_IMAGE = "static/images/SH_spawning_calendar.png"
 
-    return render_template('index.html', fish_list=all_fish_list,
-                           uncaught_fish=uncaught,
-                           image_url=CURRENT_IMAGE)
-
-
-@app.route('/fish-input')
-def fish_input():
-    return render_template('fish-input.html')
+    return render_template(
+        "index.html",
+        fish_list=all_fish_list,
+        uncaught_fish=uncaught,
+        image_url=CURRENT_IMAGE
+    )
 
 
 def get_closest_match(user_in: str, threshold: int = 80):
@@ -163,7 +199,12 @@ def get_problems(input_fish: list[str]) -> set[str]:
     return problem_children
 
 
-@app.route('/process', methods=['POST'])
+@app.route("/fish-input")
+def fish_input():
+    return render_template("fish-input.html")
+
+
+@app.route("/process", methods=["POST"])
 def process():
     data = request.data.decode("utf-8")  # Get raw text
 
@@ -193,10 +234,13 @@ def process():
         input_fish_list)
 
     # logging.debug("All fish list: %s", all_fish_list)
-    return render_template('index.html', fish_list=all_fish_list,
-                           uncaught_fish=uncaught,
-                           image_url=CURRENT_IMAGE)
+    return render_template(
+        "index.html",
+        fish_list=all_fish_list,
+        uncaught_fish=uncaught,
+        image_url=CURRENT_IMAGE
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
