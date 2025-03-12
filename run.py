1920c34706fb4ca1b26f958d4bb78fc79973ae57
Jukelyn <viperkillergaming@gmail.com>
2025-03-12 04:50:23 -0400

    Huge refactoring

---
+++ run.py
@@ -0,0 +1,17 @@
+"""
+This script runs the application defined in the `app` module from the `src`
+package.
+
+The script checks if it is being run as the main module and, if so, starts the
+application server.
+
+Modules:
+    src.app: The application instance to be run.
+
+Usage:
+    Run this script directly to start the application server.
+"""
+from src import app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
---
+++ src/__init__.py
@@ -0,0 +1,39 @@
+# pylint: disable=E0401
+"""
+This script initializes a Flask application, loads environment variables,
+and registers routes.
+
+The script performs the following tasks:
+1. Loads environment variables from a .env file using `load_dotenv()`.
+2. Creates a Flask application instance with specified template and static
+    folders.
+3. Configures the Flask application with a secret key from the environment
+    variables.
+4. Registers application routes using a custom `register_routes` function.
+
+Modules:
+     os: Provides a way of using operating system dependent functionality.
+     flask: A micro web framework for Python.
+     dotenv: Reads key-value pairs from a .env file and can set them as
+                environment variables.
+     src.routes: A module containing the `register_routes` function to
+                     register routes for the Flask app.
+
+Attributes:
+     app (Flask): The Flask application instance.
+"""
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from src.routes import register_routes
+
+load_dotenv()
+
+app = Flask(__name__,
+            template_folder="../templates",
+            static_folder="../static")
+
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+
+register_routes(app)
